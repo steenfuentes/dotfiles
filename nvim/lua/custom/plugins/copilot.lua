e9f9aa575c5df585e3eace6e08e6645076d79887
Seth Steen-Fuentes <steenfuentes@gmail.com>
2025-04-20 13:39:51 -0700

    update copilot accept. cpp lint

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,10 +1,11 @@
 return {
   'github/copilot.vim',
   config = function()
-    vim.keymap.set('i', '<A-i>', 'copilot#Accept("")', {
+    vim.keymap.set('i', '<C-c><C-a>', 'copilot#Accept("")', {
       expr = true,
       replace_keycodes = false,
     })
     vim.g.copilot_no_tab_map = true
+    github_copilot = require 'copilot'
   end,
 }
